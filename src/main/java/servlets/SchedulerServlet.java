1469501e981ebdba6fc53f66916b8547456c2138
Pilach <jakub.pilachowski@xle.pl>
2019-03-14 14:57:54 +0100

    poprawki

--- src/main/java/servlets/SchedulerServlet.java
+++ src/main/java/servlets/SchedulerServlet.java
@@ -16,15 +16,12 @@ import java.io.IOException;
 @WebServlet("/scheduler")
 public class SchedulerServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        HttpSession session = request.getSession();
-
         RateSchedulerDetails rateSchedulerDetails = (RateSchedulerDetails) request.getAttribute("rateSchedulerDetails");
 
         IRateScheduler rateScheduler = rateSchedulerDetails.getInterestRateType().equals("desc") ? new DescRateScheduler() : new ConstRateScheduler();
         rateScheduler.generateInterestRateList(rateSchedulerDetails);
 
         request.setAttribute("interestRateList", rateScheduler.getInterestRateList());
-        session.setAttribute("interestRateList", rateScheduler.getInterestRateList());
         request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
